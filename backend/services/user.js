994d569e23aacfa90d388bb1c7037a68d15e3f4e
B1GD3VD0G <devin.peevy@outlook.com>
2024-03-06 22:36:56 -0800

    routers are super simple, just utilizing the mongoose functions we defined.

--- backend/services/user.js
+++ backend/services/user.js
@@ -1,146 +1,72 @@
+const { createUser, deleteUser, updateUser, searchUsersByUsername } = require('../mongoose/functions/user_functions');
+
 // Import express router for HTTP requests.
-const router = require("express").Router();
-// Import mongoose model for user info
-const UserModel = require("../models/user_model");
+const router = require('express').Router();
 
-const { hash, generateSalt } = require("../hashing");
+// This is the default error when something unexpected happens.
+const serverError = (error) => {
+  return {
+    success: false,
+    error: error.message
+  };
+};
 
-// This must be a post method!
-// If it was get, the username and password would show up in the
-// console - obviously horrible.
-router.post("/auth", async (req, res) => {
+// createUser
+router.post('/', async (req, res) => {
   try {
-    // We're gonna search by username, as a username is unique -
-    // Passwords could potentially be the same.
-
-    // Make sure there is a body containing a username and password.
-    if (!(req.body && req.body.un && req.body.pw)) {
-      return res.status(400).json({
-        error: "Body containing username and password is required."
-      });
-    }
-    const username = req.body.un;
-
-    // Perform a search - must be an exact match!
-    const regex = new RegExp(`^${username}$`);
-
-    // This will bring in the correct username/password combination,
-    // IF the username exists in the database.
-    const correctLogin = await UserModel.findOne({
-      username: regex 
-    });
-    // If the username does NOT exist in our database, return a 404 Page Not Found.
-    // A generic error message is used to avoid leaking the existence of a valid username.
-    if (!correctLogin) {
-      return res.status(404).json({
-        error: "That username or password is incorrect."
-      });
-    }
-
-    // If we've gotten here, the username was correct! Now,
-    // correctLogin stores the valid data.
-    
-    // Check passwords.
-    const password = req.body.pw;
-    if (correctLogin.password === hash(password, correctLogin.salt)) {
-      // Username and password match! Success!
-
-      req.session.userID = correctLogin._id;
-
-      return res.status(200).json({
-        success: true
-      });
-    } else {
-      // The password was incorrect.
-      // A generic error message is used to avoid leaking the existence of a valid username.
-      return res.status(404).json({
-        error: "That username or password is incorrect."
-      });
-    }
+    // req check
+    const user = req.body.user ?? {};
+    // attempt creation
+    const creation = await createUser(user);
+    // configure res
+    return res.status(creation.code).json(creation);
   } catch (error) {
-    console.error(`Error: ${error}`);
-    res.status(500).json({
-      error: "There has been an error searching database."
-    });
+    // This shouldn't happen.
+    return res.status(500).json(serverError(error));
   }
 });
 
-router.post("/new", async (req, res) => {
+// deleteUser
+router.delete('/', async (req, res) => {
   try {
-    // Make sure there is a body containing the required fields.
-    if (!(req.body && req.body.username && req.body.password && req.body.email)) {
-      return res.status(400).json({
-        error: "Body containing username, password, and email is required."
-      });
-    }
-
-    if (await UserModel.exists({ username: req.body.username })) {
-      return res.status(409).json({ error: "Username " + req.body.username + " is already taken." });
-    }
-
-    const salt = generateSalt();
-    req.body.password = hash(req.body.password, salt);
-    req.body.salt = salt;
-
-    const user = new UserModel(req.body);
-    await user.save();
-
-    req.session.userID = user._id;
-
-    return res.status(201);
-
+    // req check
+    const login = req.body.login ?? {};
+    // attempt deletion
+    const deletion = await deleteUser(login);
+    // configure res
+    return res.status(deletion.code).json(deletion);
   } catch (error) {
-    console.error(`Error: ${error}`);
-    res.status(500).json({
-      error: "There has been an error creating the account."
-    });
+    return res.status(500).json(serverError(error));
   }
 });
 
-// Route 3: Get the public user information by searching them by username.
-// This is their username, name, and email, for now.
-// Note: this seems trivial as far as logging in goes, BUT it has future implications: adding friends, for example.
-router.get("/:username", async (req, res) => {
+// updateUser
+router.put('/', async (req, res) => {
   try {
-    // We're gonna search by username, as a username is unique -
-    // Passwords could potentially be the same.
-
-    // Make sure there is a username to search for.
-    const username = req.params.username;
-    if (!username) {
-      return res.status(400).json({
-        error: "Username is a required field! Please try again."
-      });
-    }
-
-    // Perform a search - case insensitive.
-    const regex = new RegExp(`^${username}$`, 'i');
-
-    // This will bring in the correct username/password combination,
-    // IF the username exists in the database.
-    const user = await UserModel.findOne({
-      username: regex 
-    });
-    if (!user) {
-      return res.status(404).json({
-        error: `User ${username} does not exist. Please try again.`
-      });
-    }
-
-    // If we've gotten here, the username was correct! Now,
-    // return the public user data.
-    res.status(200).json({
-      id: user._id,
-      username: user.username,
-      email: user.email
-    });
-
+    // req check
+    const login = req.body.login ?? {};
+    const changes = req.body.changes ?? {};
+    // attempt update
+    const update = await updateUser(login, changes);
+    // configure res
+    return res.status(update.code).json(update);
   } catch (error) {
-    console.error(`Error: ${error}`);
-    res.status(500).json({
-      error: "There has been an error searching database."
-    });
+    return res.status(500).json(serverError(error));
   }
-});
+})
+
+// searchUsersByUsername
+router.get('/:un', async (req, res) => {
+  try {
+    // req check
+    const un = req.params.un ?? '';
+    // attempt search
+    const search = await searchUsersByUsername(un);
+    // configure res
+    return res.status(search.code).json(search);
+  } catch (error) {
+    return res.status(500).json(serverError(error));
+  }
+})
 
 module.exports = router;
