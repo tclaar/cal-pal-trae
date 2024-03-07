ee8f1026a60ec55ec6e8a8f1016b442372920bfd
B1GD3VD0G <devin.peevy@outlook.com>
2024-03-06 22:33:11 -0800

    reconfigured the structure that we're using for models.

---
+++ backend/mongoose/schema/user_schema.js
@@ -0,0 +1,15 @@
+const { Schema } = require('mongoose');
+
+const userSchema = new Schema(
+  {
+    username: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    salt: { type: String, required: true },
+    email: { type: String, required: true }
+  },
+  {
+    collection: 'standard_users'
+  }
+);
+
+module.exports = userSchema;
